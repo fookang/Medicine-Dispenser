22592c22ba080c8f7b0b0b6396223b696acdc14a
fookang <fookang3@gmail.com>
2026-03-24 13:48:31 +0800

    Add documentation

--- Medicine_Dispenser/source/buzzer.c
+++ Medicine_Dispenser/source/buzzer.c
@@ -8,29 +8,28 @@
 /* Binary semaphore used to wake the buzzer task from other contexts. */
 SemaphoreHandle_t buzzerSem;
 
-
 static void buzzerTask(void *arg)
 {
     (void)arg;
 
-    while(1)
+    while (1)
     {
-    	if (xSemaphoreTake(buzzerSem, portMAX_DELAY) == pdTRUE)
-		{
-			buzzer_toggle();
-		}
-	}
+        if (xSemaphoreTake(buzzerSem, portMAX_DELAY) == pdTRUE)
+        {
+            buzzer_toggle();
+        }
+    }
 }
 
-/* 
+/*
  *Configures buzzer GPIO and creates buzzer task and semaphore.
  */
 void Buzzer_Init(void)
 {
-	// Turn on clock gating
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+    // Turn on clock gating
+    SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
 
-	// Set GPIO
+    // Set GPIO
     PORTE->PCR[BUZZER] &= ~PORT_PCR_MUX_MASK;
     PORTE->PCR[BUZZER] |= PORT_PCR_MUX(1);
 
@@ -41,24 +40,36 @@ void Buzzer_Init(void)
     GPIOE->PCOR |= 1 << BUZZER;
 
     buzzerSem = xSemaphoreCreateBinary();
-	xTaskCreate(buzzerTask, "buzzer", configMINIMAL_STACK_SIZE+100, NULL, 2, NULL);
+    xTaskCreate(buzzerTask, "buzzer", configMINIMAL_STACK_SIZE + 100, NULL, 2, NULL);
 }
 
+/*
+ * Turns the buzzer on.
+ */
 void buzzer_on(void)
 {
     GPIOE->PSOR |= 1 << BUZZER;
 }
 
+/*
+ * Turns the buzzer off.
+ */
 void buzzer_off(void)
 {
     GPIOE->PCOR |= 1 << BUZZER;
 }
 
+/*
+ * Toggles the buzzer state.
+ */
 void buzzer_toggle(void)
 {
     GPIOE->PTOR |= 1 << BUZZER;
 }
 
+/*
+ * Wakes the buzzer task to toggle the buzzer.
+ */
 void buzzer_wake(void)
 {
     xSemaphoreGive(buzzerSem);
