cce87f6fae035f5f3800cdfbc136519c8566abde
fookang <fookang3@gmail.com>
2026-04-08 23:09:49 +0800

    Add button interrupt for heartbeat sensor

--- Medicine_Dispenser/source/interrupt.c
+++ Medicine_Dispenser/source/interrupt.c
@@ -1,6 +1,7 @@
 #include "board.h"
 #include "button.h"
 #include "ultrasonic.h"
+#include "heartbeat.h"
 
 void PORTA_IRQHandler(void)
 {
@@ -24,6 +25,21 @@ void PORTC_PORTD_IRQHandler(void)
 
     // ISR for ultrasonic echo pin interrupt
     Ultrasonic_PORTC_IRQHandler(flags, &hpw);
+
+    // ISR for button interrupt
+    Button_PORTC_ISR(flags, &hpw);
+
+    portYIELD_FROM_ISR(hpw);
+}
+
+void ADC0_IRQHandler(void)
+{
+	NVIC_ClearPendingIRQ(ADC0_IRQn);
+    BaseType_t hpw = pdFALSE;
+    uint32_t adcValue = ADC0->R[0];
+
+    // ISR for heartbeat adc interrupt
+    Heartbeat_ADC0_IRQHandler(adcValue, &hpw);
 
     portYIELD_FROM_ISR(hpw);
 }
